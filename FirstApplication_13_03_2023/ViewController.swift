4bd0bc10a71e4fe553f0c11520671d5d057dd606
aishwarya <ashp17star@gmail.com>
2023-03-20 11:39:10 +0530

    Arrays, dictionaries, guard let, for in and forEach, ranges

--- FirstApplication_13_03_2023/ViewController.swift
+++ FirstApplication_13_03_2023/ViewController.swift
@@ -8,16 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var colorDictionary : [String : String] = ["1" : "Yellow",
+    
+    //homogenous array
+    var names : [String] = ["Shraddha","Susmita","Vaishnavi","Priyanka"]
+    var numbers = [12,34,45,46,89,90]
+    
+    //heterogenous array
+    var studentInfo : [Any] = ["12",23,"Priyanka",34.23]
+    
+    //homogeneous dictionary
+    var colorDictionary : Dictionary = ["1" : "Yellow",
                                         "2" : "Red",
                                         "3" : "Green"]
+    
+    /*var colorDictionary : [String : String] = ["1" : "Yellow",
+                                        "2" : "Red",
+                                        "3" : "Green"]*/
+    /*var colorDictionary = ["1" : "Yellow",
+                            "2" : "Red",
+                            "3" : "Green"]*/
+    
+    var dictionary : [Int : Int] = [1 : 102,
+                                    2 : 103,
+                                    3 : 104]
+    var d1 : [Int : String] = [1 : "Tushar",
+                               2 : "Rutik",
+                               3 : "Vivek"]
+    
+    //heterogenous dictionary
+    var studentDictionary : [String : Any] = ["firstName" : "Tushar", "rollNumber" : 1, "fees" : 2390.07]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         basics()
+        //checkingKey(key: "1")
+       
     }
     
     func basics(){
+        //for...in loops
+        for n in numbers{
+            print("n is \(n)")
+        }
+        
+        //forEach
+        numbers.forEach { n in
+            print(n * 5)
+        }
+        
+        //ranges
+        for number in 50...60{
+            if(number%2 == 0){
+                print("number : \(number) -- Even")
+            } else {
+                print("number is \(number) -- Odd")
+            }
+        }
+        
         var i : Int
         i = 11
         print("The value of i \(i)")
@@ -78,5 +125,15 @@ class ViewController: UIViewController {
         } else {
             print("The value of color found is nil")
         }
+    }
+    
+    
+    func checkingKey(key : String){
+        
+        guard let valueOfKeyFound = colorDictionary[key] else {
+            print("The key not found")
+            return
+        }
+        print("The key is \(key) and the value for key \(valueOfKeyFound)")
     }
 }
